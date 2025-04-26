ac51813cb35806e582350ff57523913ad0e3eb43
umich2020 <jimson@umich.com>
2025-04-25 21:10:58 -0400

    struggling with api loading
    i literally forgot how to do this, I'm able to access the api
    however when i return a function it gives me a promised object.
    idk if i should return a state with the pokemon list or power through
    to get the url through the promised object. Both are viable
    the extra url object list feels easy but could lead to more bugs later
    on

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,22 @@ export function App() {
 const [allPokemon, setAllPokemon] = useState(["ditto","squirtle","eevee"])
 const [clicked, setClicked] = useState([])
 
-const url = "https://pokeapi.co/api/v2/pokemon/ditto"
-const someething = "we set this to the url to get the image(this is part of the function"
-fetch(url, {mode: 'cors'})
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(response) {
-        console.log(response["sprites"]["other"]["official-artwork"]["front_default"])
-    })
+async function getApiLink(name="ditto"){
+    console.log("get api link is running")
+    const url = "https://pokeapi.co/api/v2/pokemon/"+name
+    let result = ""
+    await fetch(url, {mode: 'cors'})
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(response) {
+            result = response["sprites"]["other"]["official-artwork"]["front_default"]
+        })
+    console.log('result is ')
+    console.log(result)
+    return result
+}
+
     return (
         <>
         <h1>Pokemon Memory Game</h1>
@@ -21,8 +28,16 @@ fetch(url, {mode: 'cors'})
         <h2>Instructions:</h2>
         <p>Get poitns by clicking on an image. But don't click on a pokemon more than once!</p>
         </div>
+        {allPokemon.map((pokemonName)=>{
+            console.log('idk')
+                        
+                            console.log(getApiLink(pokemonName))
 
-        <Card></Card>
+            return (
+            <Card key={pokemonName} apiLink={getApiLink(pokemonName)}></Card>
+            )
+        })}
+        
         </>
     )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,10 +1,13 @@
 
 export function Card(apiLink){
-
+    console.log('api link is')
+    console.log(apiLink)
     return (
         <>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Smiley.svg/1024px-Smiley.svg.png" width="200" height="200" alt="pokemon"></img>
+        <div className="card">
+        <img src={apiLink} width="200" height="200" alt="pokemon"></img>
         <h2>Pokemon name</h2>
+        </div>
         </>
     )
 }

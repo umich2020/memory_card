f11841133380846ac95986d631007828f260eacf
umich2020 <jimson@umich.com>
2025-05-01 14:08:14 -0400

    able to show clicks
    now next step is to randomize when clicking.
    and then after that I have to pass a prop. which shows the function
    to check whether it's in the state of already clicked(i'll do a console)
    if it's already clicked, and then reset the state, this funct is a prop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { Card } from "./card"
+import './styles.css'
 
 export function App() {
 console.log("start")
@@ -10,67 +11,6 @@ const [allPokemon, setAllPokemon] = useState({
 })
 const [clicked, setClicked] = useState([])
 
-async function getApiLink(pokemon){
-    const pokemonName = pokemon.name
-    console.log('pokemon name is')
-    console.log(pokemon.name)
-
-    console.log('but pokemon is')
-    console.log(pokemon)
-    const url = "https://pokeapi.co/api/v2/pokemon/"+pokemon
-    await fetch(url, {mode: 'cors'})
-    //so allegeldy this return a promise. then i json it right?
-    //and then cuz response just a promise i think
-
-        .then(function(response){
-            console.log("allPokemon is")
-            console.log(allPokemon)
-            console.log('response is')
-            console.log(response)
-            return response.json()
-        })
-        .then(function(response) {
-            const responseUrl = response["sprites"]["other"]["official-artwork"]["front_default"]
-            // console.log('response url is')
-            // console.log(responseUrl)=> yeah this works
-
-            // setAllPokemon((prevPokemon)=> ({...prevPokemon, ditto:responseUrl}))
-            // setAllPokemon((prevPokemon)=> ({...prevPokemon, squirtle[url]:responseUrl}))
-            // console.log("allPokemon is")
-            // console.log(allPokemon)
-
-
-            const newUrl = {...allPokemon.pokemon,url:responseUrl} 
-            console.log('new url object is ')
-            console.log(newUrl)
-            console.log('what is pokemon')
-            console.log(pokemon)
-            console.log(typeof pokemon)            
-            console.log('what is pokemon url')
-            console.log(pokemon.name)
-            // console.log(typeof pokemon)
-            const newPokemon = {...allPokemon, [pokemon]:newUrl}
-            // return newPokemon
-            setAllPokemon((prevPokemon)=>({...prevPokemon,[pokemon.url]:responseUrl}))
-
-        })
-}
-// useEffect(()=>{
-//     let promises = "test"
-//     console.log(promises)
-//     // eslint-disable-next-line no-unexpected-multiline
-//     (async () => {
-//         for( const pokemon in allPokemon) {
-//             const url = "https://pokeapi.co/api/v2/pokemon/"+pokemon
-//              const pokemonPromise = await fetch(url, {mode: 'cors'})
-//              promises.push(pokemonPromise)
-//     }
-//     })();
-
-// console.log('promises is ')
-// console.log(promises)
-
-// },[])
 useEffect(()=>{
     let promises = []
     let obj = null
@@ -89,7 +29,7 @@ useEffect(()=>{
                     
 
         }
-        Promise.all(promises).then((values)=>{
+        Promise.all(promises).then(()=>{
             console.log("the value of each obj is")
             console.log(obj)
             setAllPokemon(obj)
@@ -117,14 +57,9 @@ const loadPokemon = () => {
         <h2>Instructions:</h2>
         <p>Get points by clicking on an image. But don't click on a pokemon more than once!</p>
         </div>
-        {/* {console.log("help me")
-                    for (const pokemon in allPokemon){
-                        return  (<> <Card key={allPokemon.pokemon} apiLink={allPokemon.pokemon.url}></Card> )
-                        </>}
-        } */}
+        <div id="cards">
 
-    {console.log("this is start of console")}
-    {console.log()}
+  
             {
                 Object.keys(allPokemon).map(pokemon => {
                    
@@ -133,25 +68,9 @@ const loadPokemon = () => {
                 })
                 
             }
-            {/* {
-                for(const pokemon in allPokemon) {
-                    <Card key={[pokemon]} apiLink={allPokemon.[pokemon].url}></Card>
-                }
-            } */}
-        <Card key={allPokemon.ditto.name} apiLink={allPokemon.ditto.url} name="ditto"></Card>
-        {/* {allPokemon.map((pokemon) => (
-            <h2 key={pokemon}>{pokemon}</h2>
-        ))} */}
-        {/* <div>{loadPokemon()}</div> */}
+        </div>
+            
         </>
     )
 }
-        // {allPokemon.map((pokemonName)=>{
-        //     // console.log('what im seeing')
-        //     // getApiLink(pokemonName)
-        //     // console.log(tempState)
-
-        //     return (
-        //     <Card key={pokemonName.name} apiLink={pokemonName.url}></Card>
-        //     )
-        // })}
+  
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,17 @@
 
 export function Card({apiLink,name}){
-    console.log("api link is passing to src")
-    console.log(apiLink)
+
     let srcUrl = apiLink
+
+    function handleClick (e) {
+        console.log("this is")
+        console.log(e.target.key)
+    }
+
+
     return (
         <>
-        <div className="card">
+        <div className="card" onClick={handleClick}>
         <img src={srcUrl} width="200" height="200" alt="pokemon"></img>
         <h2>{name}</h2>
         </div>

cf39bf577399087eebe23c6587c5b96bb73c3c24
umich2020 <jimson@umich.com>
2025-05-04 11:29:05 -0400

    array of obj, instead of prop of prop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,36 +4,49 @@ import './styles.css'
 
 export function App() {
 console.log("start")
-const [allPokemon, setAllPokemon] = useState({
-    ditto:{name:"ditto1", url:""},
-    squirtle:{name:"squirtle1", url:""},
-    eevee:{name:"eevee1", url:""}
-})
+const [allPokemon, setAllPokemon] = useState([
+    {name:"ditto", url:""},
+    {name:"squirtle", url:""},
+    {name:"eevee", url:""},
+    {name:"charizard"},
+])
 const [clicked, setClicked] = useState([])
+const [reRender, setreRender]= useState(0)
 
 useEffect(()=>{
     console.log("render is being used")
     let promises = []
     let obj = null
+    
     async function apiCalls() {
-        for( const pokemon in allPokemon) {
-                const url = "https://pokeapi.co/api/v2/pokemon/"+pokemon
+        for( const pokemon of allPokemon) {
+            console.log("api call pokemon")
+            console.log(pokemon.name)
+                const url = "https://pokeapi.co/api/v2/pokemon/"+pokemon.name
+               
                     await fetch(url, {mode: 'cors'})
                     .then(function(response){
                         return response.json()
                     })
                     .then(function(response) {
                         const responseUrl = response["sprites"]["other"]["official-artwork"]["front_default"]
-                        promises.push([[pokemon],{"name":pokemon,"url":responseUrl}])
-                        obj = Object.fromEntries(promises)
+                       
+                        promises.push({name:pokemon.name,url:responseUrl})
+                        console.log('what is promises')
+                        console.log(promises)
+                        // obj = Object.fromEntries(promises)
+                        // console.log('what is object')
+                        // console.log(obj)
                     })
                     
 
         }
         Promise.all(promises).then(()=>{
-            console.log("the value of each obj is")
-            console.log(obj)
-            setAllPokemon(obj)
+            console.log("the value of each promise is")
+            console.log(promises)
+            setAllPokemon(promises)
+            console.log("after setting poki")
+            console.log(allPokemon)
         })
         console.log("all pokemon is now")
         console.log(allPokemon)
@@ -41,7 +54,7 @@ useEffect(()=>{
     apiCalls()
 
 
-},[])
+},[reRender])
 // getApiLink("ditto")
 console.log("pokemon list is")
 console.log(allPokemon)
@@ -49,7 +62,7 @@ console.log(allPokemon)
    
 // })
 function randomize(){
-    setAllPokemon({...allPokemon, pikachu:{name:"pikachu"}})
+    
 }
     return (
         <>
@@ -62,9 +75,12 @@ function randomize(){
 
   
             {
-                Object.keys(allPokemon).map(pokemon => {
-                   
-                    return <Card key={[pokemon]} clickFunction={randomize} name={pokemon} apiLink={allPokemon[pokemon].url}></Card>
+                allPokemon.map(pokemon => {
+                    console.log('all pokimon is')
+                    console.log(allPokemon)
+                    console.log("pokimon name is lmao")
+                   console.log(pokemon)
+                    return <Card key={[pokemon.name]} clickFunction={randomize} name={pokemon.name} apiLink={pokemon.url}></Card>
                     
                 })
                 
